9193d834172f0f71f81347452c78078e5ee21d5d
Bob Brown <robertbernardbrown@gmail.com>
2018-03-15 19:21:53 -0400

    adds beginning skeleton to api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,24 +1,24 @@
 var db = require("../models");
 
-// module.exports = function(app) {
+module.exports = function (app) {
 
-//     // GET route for getting all of the todos
-//     app.get("/api/todos", function(req, res) {
-  
-//     });
-  
-//     // POST route for saving a new todo. You can create a todo using the data on req.body
-//     app.post("/api/todos", function(req, res) {
-  
-//     });
-  
-//     // DELETE route for deleting todos. You can access the todo's id in req.params.id
-//     app.delete("/api/todos/:id", function(req, res) {
-  
-//     });
-  
-//     // PUT route for updating todos. The updated todo will be available in req.body
-//     app.put("/api/todos", function(req, res) {
-  
-//     });
-//   };
+    // GET route for getting all of the todos
+    app.get("/", function (req, res) {
+        
+    });
+
+    // POST route for saving a new todo. You can create a todo using the data on req.body
+    app.get("/shop", function (req, res) {
+
+    });
+
+    // DELETE route for deleting todos. You can access the todo's id in req.params.id
+    app.get("/shop/:id", function (req, res) {
+
+    });
+
+    // PUT route for updating todos. The updated todo will be available in req.body
+    app.post("/contact", function (req, res) {
+
+    });
+};
