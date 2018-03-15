c7cdf9052f7d02b0e72d2fc4fe0bda0f2a5b953b
Bob Brown <robertbernardbrown@gmail.com>
2018-03-15 19:52:41 -0400

    adds sequelize syntax to skeleton

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,25 +4,30 @@ module.exports = function (app) {
 
     //getItemsFromDB is just a stand in function until we have the models ready
     app.get("/shop", function (req, res) {
-        res.getItemsFromDB( function (data) {
-            let productsFromDBForHandlebars = {
+        db.products.findAll({}).then(data => {
+            let allProductsForHandlebars = {
                 product: data
             }
-            res.render("shop", productsFromDBForHandlebars);
-        });
+            res.render("shop", allProductsForHandlebars);
+        })
     });
 
     app.get("/shop/:id", function (req, res) {
-        let param = req.params.product_id
-        res.getSpecificItem(param, function (data) {
+        let param = req.params.product_id;
+        res.products.findOne({
+            where: {
+                product_id: param
+            }
+        }).then( (data) => {
             let specificProduct = {
                 product: data
             }
-            res.render("modal", specificProduct);
+            res.render("modalView", specificProduct);
         })
     });
 
     app.post("/contact", function (req, res) {
-
+        let contactInfo = req.body.contact;
+        db.contact.create(req.body.contact);
     });
-};
+}
