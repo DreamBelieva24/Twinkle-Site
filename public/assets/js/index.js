bc1dc42c260b8f0720646b634de9239310c67200
Bob Brown <robertbernardbrown@gmail.com>
2018-03-20 14:19:23 -0400

    beautifies html files, works on getting js up

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -16,62 +16,6 @@ $('.carousel').carousel('set', 4);
 // Destroy carousel
 $('.carousel').carousel('destroy');
 
-$("#contact-button").on("click", function () {
-  let newContact = {
-      first_name: $(".first-name").val(),
-      last_name: $(".last-name").val(),
-      address: $(".address").val(),
-      email: $(".email").val(),
-      phone: $(".phone").val(),
-      subject: $(".subject").val(),
-      message: $(".message").val(),
-  }
-  $.ajax("/contact", {
-    type: "POST",
-    data: newContact
-  }).then(
-    function() {
-      location.reload();
-    }
-  );
-})
-
-// $("#contact-button").on("click", function () {
-//     let newContact = {
-//         first_name: $(".first-name").val(),
-//         last_name: $(".last-name").val(),
-//         address: $(".address").val(),
-//         email: $(".email").val(),
-//         phone: $(".phone").val(),
-//         subject: $(".subject").val(),
-//         message: $(".message").val(),
-//     }
-//     $.ajax("/contact", {
-//       type: "POST",
-//       data: newContact
-//     }).then(
-//       function() {
-//         location.reload();
-//       }
-//     );
-// })
-
-// let id = $(this).data("id");
-//     let newVisit = $(this).data("newvisit");
-//     let newVisitState = {
-//       visited: newVisit
-//     };
-//     $.ajax("/bucketlist/" + id, {
-//       type: "PUT",
-//       data: newVisitState
-//     }).then(
-//       function() {
-//         // console.log("changed visit to", newVisit);
-//         location.reload();
-//       }
-//     );
-
-
 $(document).ready(function(){
   // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
   $('.modal').modal();
