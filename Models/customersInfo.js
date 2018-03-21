f54c34959acd469ef05ad38c344e9889c90577ab
Bob Brown <robertbernardbrown@gmail.com>
2018-03-21 14:12:49 -0400

    adds validation to backend with sequelize

--- Models/customersInfo.js
+++ Models/customersInfo.js
@@ -8,28 +8,53 @@ var CustomerInfo = sequelize.define("CustomerInfo", {
 //     type: Sequelize.INTEGER
 //   },
   first_name: {
-    type: DataTypes.STRING
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      isAlpha: true,
+      notEmpty: true,
+    }
   },
   last_name: {
-    type: DataTypes.STRING
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      isAlpha: true,
+      notEmpty: true,
+    }
   },
  address: {
-    type: DataTypes.STRING
+    type: DataTypes.STRING,
+    validate: {
+      
+    }
   },
   email: {
     type: DataTypes.STRING,
+    allowNull: false,
     validate: {
-      isEmail: true
+      notEmpty: true,
     }
   },
   phone: {
-    type: DataTypes.STRING
+    type: DataTypes.STRING,
+    validate: {
+      not: ["[a-z]",'i']
+    }
   },
   subject: {
-    type: DataTypes.STRING
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    }
   },
   message: {
-    type: DataTypes.TEXT
+    type: DataTypes.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    }
   }
 });
 
