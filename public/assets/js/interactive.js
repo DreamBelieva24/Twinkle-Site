3501c4c9b4df0b0f1853db7d0a652a8b44c06707
Bob Brown <robertbernardbrown@gmail.com>
2018-03-21 20:35:15 -0400

    beautifies files

--- public/assets/js/interactive.js
+++ public/assets/js/interactive.js
@@ -1,75 +1,48 @@
 $(document).ready(function () {
   $("form#contact")
-  .validate({
-    errorElement: "div",
-    rules: {
-      first_name: {
-        required: true,
-        minlength: 2,
+    .validate({
+      errorElement: "div",
+      rules: {
+        first_name: {
+          required: true,
+          minlength: 2,
+        },
+        last_name: {
+          required: true,
+          minlength: 2
+        },
+        phone: {
+          minlength: 3
+        },
+        address: {
+          rangelength: [3, 250]
+        },
+        email: {
+          required: true,
+          minlength: 2,
+          email: true,
+        },
+        subject: {
+          required: true,
+          minlength: 2
+        },
+        message: {
+          required: true,
+          minlength: 2
+        }
       },
-      last_name: {
-        required: true,
-        minlength: 2
-      },
-      phone: {
-        minlength: 3
-      },
-      address: {
-        rangelength:[3,250]
-      },
-      email: {
-        required: true,
-        minlength: 2,
-        email: true,
-      },
-      subject: {
-        required: true,
-        minlength: 2
-      },
-      message: {
-        required: true,
-        minlength: 2
+      submitHandler: function (form) {
+        $.ajax("/contact", {
+          type: "POST",
+          data: $(form).serialize(),
+          success: function (res) {
+            if (res.errors) {
+              $('#modal1').modal('open');
+            } else {
+              location.reload();
+            }
+          },
+        });
       }
-    },
-    submitHandler: function(form) {
-      $.ajax("/contact", {
-        type: "POST",
-        data: $(form).serialize(),
-        success: function(res) {
-          if (res.errors) {
-            $('#modal1').modal('open');
-          } else {
-            location.reload();
-          }
-        },   
-      });
-  }
-  })
-  // .submit(function (e) {
-  //   e.preventDefault();
-  //   let newContact = {
-  //     first_name: $("#first_name").val(),
-  //     last_name: $("#last_name").val(),
-  //     address: $("#address").val(),
-  //     email: $("#email").val(),
-  //     phone: $("#phone").val(),
-  //     subject: $("#subject").val(),
-  //     message: $("#message").val(),
-  //   }
-  //   $.ajax("/contact", {
-  //     type: "POST",
-  //     data: newContact
-  //   }).then(
-  //     function (data) {
-
-  //       //backend validation spit out here
-  //       console.log(data);
-  //       if(data.error) {
-  //         //modal message of error
-  //       } else {
-  //         location.reload();
-  //       }
-  //     }
-  //   );
-  // });
+    })
 });
