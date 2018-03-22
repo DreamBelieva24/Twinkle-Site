8be37a4ff5666d527521f3d974ae511de6fcef34
katophelix <katharrismail@gmail.com>
2018-03-22 18:25:53 -0400

    dropdown buttons

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -16,8 +16,44 @@ $('.carousel').carousel('set', 4);
 // Destroy carousel
 $('.carousel').carousel('destroy');
 
+
+
+// Or with jQuery
+
+// $('.carousel').carousel({
+//   fullWidth: true
+// });
+
 $(document).ready(function(){
   // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
   $('.modal').modal();
 });
      
+
+
+
+  // Or with jQuery
+
+  // $('.dropdown-trigger').dropdown();
+
+  // var instance = M.Dropdown.getInstance(elem);
+
+ 
+
+  //   $('.dropdown-trigger').dropdown('methodName');
+  //   $('.dropdown-trigger').dropdown('methodName', paramName);
+
+    $('.dropdown-button').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrain_width: false, // Does not change width of dropdown to that of the activator
+      hover: true, // Activate on hover
+      gutter: 0, // Spacing from edge
+      belowOrigin: false, // Displays dropdown below the button
+      alignment: 'left' // Displays dropdown with edge aligned to the left of button
+    }
+  );
+  
+  $(document).ready(function() {
+    $('select').material_select();
+ });
