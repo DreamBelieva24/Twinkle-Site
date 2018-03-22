09afc0d95fe5b581993f645c600309f16af00707
Bob Brown <robertbernardbrown@gmail.com>
2018-03-22 19:40:24 -0400

    sets up JawsDB

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,29 +2,28 @@ var db = require("../Models");
 
 module.exports = function (app) {
 
-    //just a stand in function until we have the models ready
-    // app.get("/shop", function (req, res) {
-    //     db.Inventory.findAll({}).then(data => {
-    //         let allProductsForHandlebars = {
-    //             product: data
-    //         }
-    //         res.json(allProductsForHandlebars);
-    //     })
-    // });
+    app.get("/shop", function (req, res) {
+        db.Inventory.findAll({}).then(data => {
+            let allProductsForHandlebars = {
+                product: data
+            }
+            res.json(allProductsForHandlebars);
+        })
+    });
 
-    // app.get("/shop/:id", function (req, res) {
-    //     let param = req.params.id;
-    //     db.Inventory.findOne({
-    //         where: {
-    //             id: param
-    //         }
-    //     }).then( (data) => {
-    //         let specificProduct = {
-    //             product: data
-    //         }
-    //         res.json(specificProduct);
-    //     })
-    // });
+    app.get("/shop/:id", function (req, res) {
+        let param = req.params.id;
+        db.Inventory.findOne({
+            where: {
+                id: param
+            }
+        }).then( (data) => {
+            let specificProduct = {
+                product: data
+            }
+            res.json(specificProduct);
+        })
+    });
 
     app.post("/contact", function (req, res, next) {
         db.CustomerInfo.create(req.body).then(data => {
