c74f4d76c2fe0a046339c9e0af22a029a735aac8
Bob Brown <robertbernardbrown@gmail.com>
2018-03-23 15:27:32 -0400

    tries to add dropdown to modals

--- public/assets/js/shop.js
+++ public/assets/js/shop.js
@@ -9,16 +9,31 @@ $(document).ready(function () {
             function (data) {
                 console.log(data);
                 if (data.product) {
+                    let stock = data.product.in_stock;
+                    $(".purse-info").remove();
+                    //create div to append info to within modal
                     let productDiv = $("<div>").addClass("purse-info")
                     $(".purse-info").empty();
                     $(".purse-no-stock").empty();
+                    //create block to display stock and append to div
                     let inventoryHeader = $("<h4>").text("In stock:");
-                    let inventoryStock = $("<p>").text(data.product.in_stock);
+                    let inventoryStock = $("<p>").text(stock);
                     let inventoryBlock = productDiv.append(inventoryHeader.append(inventoryStock));
-                    $(".purse-choice").append(inventoryBlock);
+                    //create dropdown to allow user to select desired quantity
+                    let quantity = $("#purse-quantity");
+                    let optionTag;
+                    for (let i = 1; i <= stock; i++) {
+                        console.log(i);
+                        optionTag = $("<option>").text(i);
+                        quantity.append(optionTag);
+                    }
+                    // let selecting = selectDiv.append(selectTag.append(optionTag));
+                    // let selectBlock = productDiv.append(quantity);
+                    //display price of purchase
                     let priceHeader = $("<h4>").text("Price:");
                     let price = $("<p>").text("$" + data.product.price);
                     let priceBlock = productDiv.append(priceHeader.append(price));
+                    $(".purse-choice").append(inventoryBlock);
                 } else {
                     let stockMessageDiv = $("<div>").addClass("purse-no-stock")
                     $(".purse-no-stock").empty();
@@ -40,16 +55,17 @@ $(document).ready(function () {
             function (data) {
                 console.log(data);
                 if (data.product) {
+                    let stock = data.product.in_stock;
                     let productDiv = $("<div>").addClass("credit-info")
                     $(".credit-info").empty();
                     $(".credit-no-stock").empty();
                     let inventoryHeader = $("<h4>").text("In stock:");
-                    let inventoryStock = $("<p>").text(data.product.in_stock);
+                    let inventoryStock = $("<p>").text(stock);
                     let inventoryBlock = productDiv.append(inventoryHeader.append(inventoryStock));
-                    $(".credit-choice").append(inventoryBlock);
                     let priceHeader = $("<h4>").text("Price:");
                     let price = $("<p>").text("$" + data.product.price);
                     let priceBlock = productDiv.append(priceHeader.append(price));
+                    $(".credit-choice").append(inventoryBlock);
                 } else {
                     let stockMessageDiv = $("<div>").addClass("credit-no-stock")
                     $(".credit-no-stock").empty();
@@ -71,16 +87,17 @@ $(document).ready(function () {
             function (data) {
                 console.log(data);
                 if (data.product) {
+                    let stock = data.product.in_stock;
                     let productDiv = $("<div>").addClass("lug-info")
                     $(".lug-info").empty();
                     $(".lug-no-stock").empty();
                     let inventoryHeader = $("<h4>").text("In stock:");
-                    let inventoryStock = $("<p>").text(data.product.in_stock);
+                    let inventoryStock = $("<p>").text(stock);
                     let inventoryBlock = productDiv.append(inventoryHeader.append(inventoryStock));
-                    $(".lug-choice").append(inventoryBlock);
                     let priceHeader = $("<h4>").text("Price:");
                     let price = $("<p>").text("$" + data.product.price);
                     let priceBlock = productDiv.append(priceHeader.append(price));
+                    $(".lug-choice").append(inventoryBlock);
                 }  else {
                     let stockMessageDiv = $("<div>").addClass("lug-no-stock")
                     $(".lug-no-stock").empty();
