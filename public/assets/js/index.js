37dbfaec9b373f94fcd2a22513a7081c4dc03abe
Jennifer Campbell <dreambelieva24@gmail.com>
2018-03-24 14:06:56 -0400

    tries id on shop

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -68,7 +68,7 @@ $(".addCart-purse").on("click", function() {
 
   productNames.push(document.getElementById("Octagon Purse").id)
   productColors.push(document.getElementById("purse-select").value)
-  productQuantities.push(parseInt(document.getElementsByClassName("chosen-quantity-purse").value))
+  productQuantities.push(parseInt(document.getElementById("chosen-quantity-purse").value))
   // productPrices.push(parseInt(document.getElementById("54.00").id))
   
   displayCart();
